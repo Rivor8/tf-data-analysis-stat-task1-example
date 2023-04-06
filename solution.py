9eec9587b9c51feb8f7d7ceac9f841dcc866d602
Aleksandr Vorobev <rivor08@gmail.com>
2023-04-06 22:19:52 +0300

    solution

--- solution.py
+++ solution.py
@@ -2,10 +2,9 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 319607269 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return x.mean() # Ваш ответ
+    n = np.arange(0, 1, 1/88)
+    r = np.multiply(x, n).sum()
+    return r / n.sum() # Ваш ответ
