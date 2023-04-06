c6d88b7bdc93193896e570843cb2680b938e5f1e
Aleksandr Vorobev <rivor08@gmail.com>
2023-04-06 23:11:51 +0300

    solution

--- solution.py
+++ solution.py
@@ -5,6 +5,4 @@ import numpy as np
 chat_id = 319607269 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    n = np.arange(0, 1, 1/88)
-    r = np.multiply(x, n).sum()
-    return r / n.sum() # Ваш ответ
+    return x.mean() / 88 # Ваш ответ
